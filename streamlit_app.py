430a393c22e227891ae6a01ea07c550626f7a46a
islehee <islehee@gmail.com>
2024-08-15 13:51:37 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,8 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
-      session.sql(my_insert_stmt).collect()
-      st.success('Your Smoothie is ordered, '+name_on_order, icon="✅")
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered, '+name_on_order, icon="✅")
 
 
 
